f6e722da1637afe9e7b30437008478e2343953cb
KnightOfTribulus <treflcrew@gmail.com>
2020-05-28 18:05:59 +0300

    experiment is ready

--- experiment.lisp
+++ experiment.lisp
@@ -19,7 +19,7 @@
 (defun test-radius ()
   (let ((pln (make-instance 'sizeless-polynomial)))
     (-<>>
-	(loop for i from 0.5 downto 0.01 by 0.005
+	(loop for i from 0.5 downto 0.005 by 0.005
 	       for new-poly = (md:modf (approx-step pln) i)
 	      collect  (list (/ 1 (expt i 2))
 			     (real-time (radius new-poly 'r1 'r2))))
@@ -45,7 +45,7 @@
 (defun test-radius-slow ()
   (let ((pln (make-instance 'sizeless-polynomial)))
     (-<>>
-	(loop for i from 0.5 downto 0.01 by 0.005
+	(loop for i from 0.5 downto 0.005 by 0.005
 	       for new-poly = (md:modf (approx-step pln) i)
 	      collect  (list (/ 1 (expt i 2))
 			     (real-time (radius-slow new-poly 'r1 'r2))))
@@ -53,13 +53,57 @@
 	(format t "~&~{~a ~}" i))))
   (fc:clear-cache-all-function-caches))
 
-
-(defun plot-speeds ()
+(defun plot-speeds (&key (output-file "experiment-1.png") (title-arg "Скорости выполнения"))
   (with-plots (*standard-output* :debug t)
     (gp-setup :terminal '(pngcairo)
-	      :output "experiment-1.png"
-	      :key '(box lt -1 lw 2 opaque))
+	      :title title-arg
+	      :output output-file
+	      ;;:key '(box lt -1 lw 2 opaque)
+	      :key '(top left))
     (gp :set :xlabel "n")
     (gp :set :ylabel "τ(n)")
-    (plot #'test-radius :with '(lines lw 5 linecolor rgb "green" title "параллельный алг."))
-    (plot #'test-radius-slow :with '(lines lw 5 lt 3 linecolor rgb "red" title "последовательный алг."))))
+    (plot #'test-radius :with '(lines lw 2 linecolor rgb "green" title "параллельный алг."))
+    (plot #'test-radius-slow :with '(lines lw 2 dashtype "_-" linecolor rgb "red" title "последовательный алг."))))
+
+(defun speedup-n (threads step)
+  (let ((lp:*kernel* (lp:make-kernel threads))
+	(pol (make-instance 'sizeless-polynomial
+			     :approx-step step)))
+    (/ (prog1
+	   (real-time (radius-slow pol 'r1 'r2))
+	 (fc:clear-cache-all-function-caches)) 
+       (prog1
+	   (real-time (radius pol 'r1 'r2))
+	 (fc:clear-cache-all-function-caches)))))
+
+(defun format-speedups ()
+  (loop for threads from 2 to 10 by 1 do
+    (format t "~&~a ~a"
+	    threads
+	    (speedup-n threads 0.01))))
+
+(defun format-speedup (step)
+  (loop for threads from 2 to 10 by 1 do
+    (format t "~&~a ~a"
+	    threads
+	    (speedup-n threads step))))
+
+(defun plot-speedup (&key (output-file "speedup-1.png") (title-arg "Ускорение"))
+  (with-plots (*standard-output* :debug t)
+    (gp-setup :terminal '(pngcairo)
+	      :title title-arg
+	      :output output-file
+	      ;;:key '(box lt -1 lw 2 opaque)
+	      :key '(top left))
+    (gp :set :xlabel "n")
+    (gp :set :ylabel "S(n)")
+    (plot #'(lambda ()
+	      (format-speedup 0.05))
+	  :with `(lines lw 2 linecolor rgb "green" title ,(format nil "n=~a" (/ 1 (expt 0.05 2)))))
+    (plot #'(lambda ()
+	      (format-speedup 0.01))
+	  :with `(lines lw 2 linecolor rgb "blue" title ,(format nil "n=~a" (/ 1 (expt 0.01 2)))))
+    (plot #'(lambda ()
+	      (format-speedup 0.005))
+	  :with `(lines lw 2 linecolor rgb "red" title ,(format nil "n=~a" (/ 1 (expt 0.005 2)))))))
+
